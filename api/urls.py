f85d790490e6477fdfbe996514e7e8eaf74c0d4a
unknown <anandbhalke05@gmail.com>
2022-12-19 14:41:57 +0530

    initial commit

---
+++ api/urls.py
@@ -0,0 +1,27 @@
+"""product URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+#path('', views.apiOverview, name='apiOverview'),
+path('product-list/', views.ShowAll, name='product-list'),
+path('product-detail/<int:pk>/', views.ViewProduct, name='product-detail'),
+path('product-create/', views.CreateProduct, name='product-create'),
+path('product-update/<int:pk>/', views.updateProduct, name='product-update'),
+path('product-delete/<int:pk>/', views.deleteProduct, name='product-delete'),
+  
+]
